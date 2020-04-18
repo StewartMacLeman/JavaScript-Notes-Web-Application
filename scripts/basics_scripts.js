4cf3e7b29cf8f4790972cba52fcf714575204920
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-04-18 17:05:46 +0100

    Basic code html and script files added.

---
+++ scripts/basics_scripts.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Variable Declaration Example.
+
+document.querySelector("#basic_btn_1").addEventListener("click", varDecEx);
+
+function varDecEx() {
+  let c = 50;
+  let d = 60;
+  alert( c + d );
+};
+// -----------------------------------------------------------------------------
