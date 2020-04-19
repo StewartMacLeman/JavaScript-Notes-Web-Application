3ebaa1c9c40170b4baedb98e8dc1d0ee02d5118c
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-04-19 10:40:46 +0100

    Other project html and scripts created.

--- scripts/array_methods.js
+++ scripts/array_methods.js
@@ -10,14 +10,3 @@ function varDecEx() {
   alert( c + d );
 };
 // -----------------------------------------------------------------------------
-
-// forEach Example.
-
-document.querySelector("#basic_btn_2").addEventListener("click", forEachExample);
-
-function forEachExample() {
-  let c = 50;
-  let d = 60;
-  [c, d].forEach(alert);
-};
-// -----------------------------------------------------------------------------
