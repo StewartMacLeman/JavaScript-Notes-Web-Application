614d8a7b5d99eacbff2583a83ff4320a9a5e0b01
Stewart MacLeman <stewart_macleman@hotmail.com>
2020-04-24 21:06:24 +0100

    Array method files updated.

--- scripts/array_methods.js
+++ scripts/array_methods.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Array Method Example 1.
+// Array Method Example 1: Return an Index Value.
 
 document.querySelector("#array_btn_1").addEventListener("click", arrayMethodEx1);
 
@@ -12,7 +12,7 @@ function arrayMethodEx1() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 2.
+// Array Method Example 2: Update an Index Value
 
 document.querySelector("#array_btn_2").addEventListener("click", arrayMethodEx2);
 
@@ -24,7 +24,7 @@ function arrayMethodEx2() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 3.
+// Array Method Example 3: A Multidimensional Array
 
 document.querySelector("#array_btn_3").addEventListener("click", arrayMethodEx3);
 
@@ -36,7 +36,7 @@ function arrayMethodEx3() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 4.
+// Array Method Example 4: push()
 
 document.querySelector("#array_btn_4").addEventListener("click", arrayMethodEx4);
 
@@ -48,7 +48,7 @@ function arrayMethodEx4() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 5.
+// Array Method Example 5: pop()
 
 document.querySelector("#array_btn_5").addEventListener("click", arrayMethodEx5);
 
@@ -60,7 +60,7 @@ function arrayMethodEx5() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 6.
+// Array Method Example 6: unshift()
 
 document.querySelector("#array_btn_6").addEventListener("click", arrayMethodEx6);
 
@@ -72,7 +72,7 @@ function arrayMethodEx6() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 7.
+// Array Method Example 7: shift()
 
 document.querySelector("#array_btn_7").addEventListener("click", arrayMethodEx7);
 
@@ -84,7 +84,7 @@ function arrayMethodEx7() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 8.
+// Array Method Example 8: toString() and join()
 
 document.querySelector("#array_btn_8").addEventListener("click", arrayMethodEx8);
 
@@ -100,19 +100,231 @@ function arrayMethodEx8() {
 };
 // -----------------------------------------------------------------------------
 
-// Array Method Example 9.
+// Array Method Example 9: split()
 
 document.querySelector("#array_btn_9").addEventListener("click", arrayMethodEx9);
 
 function arrayMethodEx9() {
   let randomString = 'Bobby,Sam,David,Mary,June';
   let stringToArray = randomString.split(',');
-  alert(typeof randomString);
-  alert(typeof stringToArray);
-  alert(stringToArray[1]);
+  alert("Alert No. 1: " + typeof randomString);
+  alert("Alert No. 2: " + typeof stringToArray);
+  alert("Alert No. 3: " + Array.isArray(stringToArray));
+  alert("Alert No. 4: " + stringToArray[1]);
 
   for(let i=0; i < stringToArray.length; i++){
     alert(stringToArray[i].toUpperCase());
   }
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 10: reverse()
+
+document.querySelector("#array_btn_10").addEventListener("click", arrayMethodEx10);
+
+function arrayMethodEx10() {
+  let numbers = [1, 2, 3, 4, 5];
+  let backwards = numbers.reverse();
+
+  alert(backwards);
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 11: slice()
+
+document.querySelector("#array_btn_11").addEventListener("click", arrayMethodEx11);
+
+function arrayMethodEx11() {
+    let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+    let fruits = fruitbowl.slice(1, 3);
+
+    alert(fruits);
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 12: splice()
+
+document.querySelector("#array_btn_12").addEventListener("click", arrayMethodEx12);
+
+function arrayMethodEx12() {
+    let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+    fruitbowl.splice(2, 0, "oranges", "peaches");
+
+    alert(fruitbowl);
+};
+
+document.querySelector("#array_btn_13").addEventListener("click", arrayMethodEx13);
+
+function arrayMethodEx13() {
+    let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+    fruitbowl.splice(2, 2, "oranges", "peaches");
+
+    alert(fruitbowl);
+};
+
+document.querySelector("#array_btn_14").addEventListener("click", arrayMethodEx14);
+
+function arrayMethodEx14() {
+    let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+    fruitbowl.splice(2, 2);
+
+    alert(fruitbowl);
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 13: sort()
+
+document.querySelector("#array_btn_15").addEventListener("click", arrayMethodEx15);
+
+function arrayMethodEx15() {
+    let fruitbowl = ["pineapples", "bananas", "mangoes", "grapes", "apples"];
+    fruitbowl.sort();
+
+    alert(fruitbowl);
+};
+
+document.querySelector("#array_btn_16").addEventListener("click", arrayMethodEx16);
+
+function arrayMethodEx16() {
+    let fruitbowl = ["pineapples", "bananas", "mangoes", "grapes", "apples"];
+    fruitbowl.sort().reverse();
+
+    alert(fruitbowl);
+};
+
+document.querySelector("#array_btn_17").addEventListener("click", arrayMethodEx17);
+
+function arrayMethodEx17() {
+    let numbers = [9, 7, 3, 5, 1, 6, 4, 2, 8];
+    numbers.sort()
+
+    alert(numbers);
+};
+
+document.querySelector("#array_btn_18").addEventListener("click", arrayMethodEx18);
+
+function arrayMethodEx18() {
+    let numbers = [100, 7, 30, 25, 1000, 16, 4, 32, 88];
+    numbers.sort()
+
+    alert(numbers);
+};
+
+document.querySelector("#array_btn_19").addEventListener("click", arrayMethodEx19);
+
+function arrayMethodEx19() {
+    let numbers = [100, 7, 30, 25, 1000, 16, 4, 32, 88];
+    numbers.sort( function(a, b) {return a-b } );
+
+    alert(numbers);
+};
+
+document.querySelector("#array_btn_20").addEventListener("click", arrayMethodEx20);
+
+function arrayMethodEx20() {
+    let numbers = [100, 7, 30, 25, 1000, 16, 4, 32, 88];
+    numbers.sort( function(a, b) {return b-a } );
+
+    alert(numbers);
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 14: Iterations
+
+document.querySelector("#array_btn_21").addEventListener("click", arrayMethodEx21);
+
+function arrayMethodEx21() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  for (let i = 0; i < fruitbowl.length; i++) {
+    alert( fruitbowl[i] );
+  }
+};
+
+document.querySelector("#array_btn_22").addEventListener("click", arrayMethodEx22);
+
+function arrayMethodEx22() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  for (let fruit of fruitbowl) {
+    alert( fruit );
+  }
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 15: forEach()
+
+document.querySelector("#array_btn_23").addEventListener("click", arrayMethodEx23);
+
+function arrayMethodEx23() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  fruitbowl.forEach(function(fruit) {return alert(fruit.toUpperCase());})
+};
+
+document.querySelector("#array_btn_24").addEventListener("click", arrayMethodEx24);
+
+function arrayMethodEx24() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  fruitbowl.forEach(fruitFunc);
+
+  function fruitFunc(fruit, indexNo){
+    return alert("Index Number " + indexNo + ": " + fruit.toUpperCase());
+  }
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 16: map()
+
+document.querySelector("#array_btn_25").addEventListener("click", arrayMethodEx25);
+
+function arrayMethodEx25() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  let newFruitBowl = fruitbowl.map(fruit => fruit.length);
+  alert(newFruitBowl);
+};
+
+document.querySelector("#array_btn_26").addEventListener("click", arrayMethodEx26);
+
+function arrayMethodEx26() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  let newFruitBowl = fruitbowl.map(wordLength);
+
+  function wordLength(fruitItem, indexNo) {
+    return alert("The word length at Index Number " + indexNo + " is "
+     + fruitItem.length + "!");
+  }
+  alert(newFruitBowl);
+};
+// -----------------------------------------------------------------------------
+
+// Array Method Example 17: filter()
+
+document.querySelector("#array_btn_27").addEventListener("click", arrayMethodEx27);
+
+function arrayMethodEx27() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  let anotherFruitBowl = fruitbowl.filter(fruit => fruit.length < 7);
+  alert(anotherFruitBowl);
+};
+
+document.querySelector("#array_btn_28").addEventListener("click", arrayMethodEx28);
+
+function arrayMethodEx28() {
+  let fruitbowl = ["apples", "bananas", "mangoes", "grapes"];
+
+  let anotherFruitBowl = fruitbowl.filter(selectedFruit);
+
+  function selectedFruit(fruitItem, indexNo) {
+    return alert("The fruit at Index Number " + indexNo + " is "
+    + (fruitItem.length < 7) + "!");
+  }
+
+  // (fruit => fruit.length < 7);
+  // alert(anotherFruitBowl);
 };
 // -----------------------------------------------------------------------------
